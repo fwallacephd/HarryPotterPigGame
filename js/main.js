9ad57987bfcaf67dcc8e2fa65ce781ac15661b64
Faith Wallace <fwallacephd@gmail.com>
2018-05-21 11:55:51 -0400

    major refactor

--- js/main.js
+++ js/main.js
@@ -1,65 +1,123 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 /* Global Scope*/
-let player1Score, player2Score, activePlayer, player1Total, player2Total, winner, dice;
 
+let activePlayer, winner, dice;
+
+let player1 = {
+  name: "player1",
+  currentScore: 0,
+  totalScore: 0,
+  totalElementID: "player1-total",
+  nameElementID: "#player1-name"
+};
+
+let player2 = {
+  name: "player2",
+  currentScore: 0,
+  totalScore: 0,
+  totalElementID: "player2-total",
+  nameElementID: "#player2-name"
+};
+
+
+
+/*Game Init - NEW GAME*/
+function gameInit() {
+
+  activePlayer = player1;
+
+  //Sets all of the values to 0.
+  document.getElementById('player1-score').textContent = "0";
+  document.getElementById('player2-score').textContent = "0";
+  document.getElementById('player1-total').textContent = "0";
+  document.getElementById('player2-total').textContent = "0";
+
+  //Sets the name of the player.
+  document.getElementById('player1-name').textContent = "Player 1";
+  document.getElementById('player2-name').textContent = "Player 2";
+
+  //Sets the initial image to the Harry Potter icon.
+  document.querySelector('.dice').src="potter.png";
+
+  //Makes the roll and hold buttons visible - they turn hidden when the game ends.
+  document.getElementById('hold').style.visibility = "visible";
+  document.getElementById('roll').style.visibility = "visible";
+};
+
+/* Next Play & Track Score function*/
+function nextPlayer() {
+
+  if (activePlayer === player1) {
+
+    updateScoreAndSwitchPlayer(player2, player1);
+
+  } else if (activePlayer === player2) {
+
+    updateScoreAndSwitchPlayer(player1, player2);
+
+  }
+};
+
+/*Switch Players and update the score*/
+function updateScoreAndSwitchPlayer(newActivePlayer, inactivePlayer) {
+
+      document.querySelector(inactivePlayer.nameElementID).classList.add('active');
+      document.querySelector(newActivePlayer.nameElementID).classList.remove('active');
+      if (dice > 1) {
+        inactivePlayer.currentScore += dice;
+        document.getElementById(inactivePlayer.totalElementID).textContent = inactivePlayer.currentScore;
+      } else {
+      activePlayer = newActivePlayer;
+      document.getElementById(inactivePlayer.totalElementID).textContent = "0";
+      document.getElementById(newActivePlayer.totalElementID).textContent = "0";
+    }
+}
+
+/*START GAME*/
 
 gameInit();
 
-/*ROll Dice*/
-document.querySelector('#roll').addEventListener('click', function() {
+/*ROll DICE*/
+document.querySelector('#roll').addEventListener('click', () => {
+
   /*Randomize*/
   dice = Math.floor(Math.random() * 6) + 1;
-  console.log("The number is" + dice);
 
   /*Display the Result*/
   /*Matching Images*/
-
-  let icon = ["egg", "broom", "owl", "hat", "map", "goblet"];
+  const icon = ["egg", "broom", "owl", "hat", "map", "goblet"];
   let image = icon[dice -1];
 
-
+  /*Update the image basd on the dice*/
   let diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = image + '.png';
 
-
 /*Change players, Update Scores - Function */
-nextPlayer();
+  nextPlayer();
 });
-
 
 /*Hold Button*/
 document.querySelector('#hold').addEventListener('click', function() {
   //Add Current Score to Total Score
-  //Update the UI
-  if (activePlayer === "player1") {
-    player1Total += player1Score;
-    document.getElementById('player1-score').textContent = player1Total;
-    activePlayer = "player2";
-    player1Score = 0;
-  } else if (activePlayer === "player2") {
-    player2Total += player2Score;
-    document.getElementById('player2-score').textContent = player2Total;
-    activePlayer = "player1";
-    player2Score = 0;
+
+  if (activePlayer === player1) {
+    player1.totalScore += player1.currentScore;
+    document.getElementById('player1-score').textContent = player1.totalScore;
+    activePlayer = player2;
+    player1.currentScore = 0;
+  } else if (activePlayer === player2) {
+    player2.totalScore += player2.currentScore;
+    document.getElementById('player2-score').textContent = player2.totalScore;
+    activePlayer = player1;
+    player2.currentScore = 0;
   }
   //Check if player won the game
-  if (player1Total >= 25) {
+  if (player1.totalScore >= 25) {
     document.getElementById('player1-name').textContent = "Winner!";
     document.querySelector('.dice').src="potter.png";
     document.getElementById('roll').style.visibility = "hidden";
     document.getElementById('hold').style.visibility = "hidden";
-  } else if (player2Total >= 25) {
+  } else if (player2.totalScore >= 25) {
     document.getElementById('player2-name').textContent = "Winner!";
     document.querySelector('.dice').src="potter.png";
     document.getElementById('roll').style.visibility = "hidden";
@@ -67,65 +125,7 @@ document.querySelector('#hold').addEventListener('click', function() {
   }
 });
 
-/*New Game*/
-function gameInit() {
-  player1Score = 0;
-  player2Score = 0;
-  player1Total = 0;
-  player2Total = 0;
-  activePlayer = "player1";
-
-
-  document.getElementById('player1-score').textContent = "0";
-  document.getElementById('player2-score').textContent = "0";
-  document.getElementById('player1-total').textContent = "0";
-  document.getElementById('player2-total').textContent = "0";
-
-  document.getElementById('player1-name').textContent = "Player 1";
-  document.getElementById('player2-name').textContent = "Player 2";
-
-  document.querySelector('.dice').src="potter.png";
-
-  document.getElementById('hold').style.visibility = "visible";
-  document.getElementById('roll').style.visibility = "visible";
-};
-
-
 document.getElementById('new-game').addEventListener('click', gameInit);
-
-/* Track Player and Score - Function*/
-function nextPlayer() {
-  if (activePlayer === "player1") {
-    //Show Active PLayer
-    document.querySelector('#player1-name').classList.add('active');
-    document.querySelector('#player2-name').classList.remove('active');
-    if (dice > 1) {
-      player1Score += dice;
-      document.getElementById('player1-total').textContent = player1Score;
-    } else {
-      //Change State Pattern
-      activePlayer = "player2";
-      document.getElementById('player1-total').textContent = "0";
-      document.getElementById('player2-total').textContent = "0";
-      //document.querySelector('.dice').style.display = "none";
-    }
-
-  } else if (activePlayer === "player2") {
-    //Show Active Player
-    document.querySelector('#player2-name').classList.add('active');
-    document.querySelector('#player1-name').classList.remove('active');
-    if (dice > 1) {
-      player2Score += dice;
-      document.getElementById('player2-total').textContent = player2Score;
-    } else {
-      //Change State Pattern
-      activePlayer = "player1";
-      document.getElementById('player2-total').textContent = "0";
-      document.getElementById('player1-total').textContent = "0";
-      //document.querySelector('.dice').style.display = "none";
-    }
-  }
-};
 
 
 
